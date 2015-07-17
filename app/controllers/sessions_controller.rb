cba9475f5c30c56e516e50e09b94064575ce8baf
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-17 14:35:43 -0700

    signup working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+	@user = User.create(user_params)
+	if @user.save
+		session[:user_id] = @user.id
+		redirect_to home_path
+	else
+		render :signup
+	end
+  end
+
+
   def signup
+  	@user = User.new
   end
 
   def login
@@ -7,4 +19,10 @@ class SessionsController < ApplicationController
 
   def home
   end
+
+  private
+  def user_params
+  	params.require(:user).permit(:username,:password,:password_digest)
+  end
+
 end
